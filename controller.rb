b3829add85bff9cd8018ab087da78aff6a2a4703
Bill Gallagher & Valerie Smith <pair+juggler434.valeriesmith@devbootcamp.com>
2014-11-14 10:49:52 -0800

    Update Controller
    -added some methods to our controller class

--- controller.rb
+++ controller.rb
@@ -1,3 +1,5 @@
+require 'asciiart'
+
 =begin
 0. User login
 I. (Display Image from View)
@@ -9,32 +11,39 @@ V. Store # Right in User.db
 =end
 
 class Controller
-  def initialize(user_id)
-    @user_id = user_id
+  def initialize(username)
+    @user = User.find_or_create_by(user_name: username)
     @all_boots = Boot.all
-  end
-
-  def validate_user
-
   end
 
   def run
     @all_boots.each do |boot|
-
+      boot.display_image
+      compare_guess
+      correct_answer?
+      update_correct_answers
     end
   end
 
   def display_image #Probably move to display later
-
-  end
-
-  def compare_guess
-
+    boot_pic = AsciiArt.new(self.image)
+    puts boot_pic.to_ascii_art
   end
 
   def correct_answer?
+    guess = gets.chomp
+    guess == boot.name
+  end
+
+  def display_result_message
+    if compare_guess
+      puts "woohoooooo"
+    else
+      puts "sorry, the correct answer is #{boot.name}."
+    end
   end
 
   def update_correct_answers
+    @user.number_correct += 1 if correct_answer?
   end
 end
